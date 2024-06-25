7905b3161c6cbcbf25f66a8b952e664a26ca0e37
Lilia Sfaxi <liliasfaxi@gmail.com>
2024-06-25 09:48:48 +0100

    Initial commit

---
+++ library/toolbox/btree.py
@@ -0,0 +1,6 @@
+class BTree:
+    def __init__(self):
+        print("Hello")
+    
+    def sum(self, a, b):
+        return a+b
---
+++ tests/toolbox/test_btree.py
@@ -0,0 +1,11 @@
+import unittest
+from library.toolbox.btree import BTree
+
+class TestBTree(unittest.TestCase):
+
+    def setUp(self):
+        self.btree = BTree()
+
+    def test_add(self):
+        self.assertEqual(self.btree.sum(2, 3), 5)
+        self.assertEqual(self.btree.sum(-1, 1), 0)
